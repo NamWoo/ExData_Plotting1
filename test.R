300826cff5d210108ba57ca5efd1856a0269206f
Nam Woo <preice@yonsei.ac.kr>
2019-02-27 10:02:13 +0900

    add

--- test.R
+++ test.R
@@ -1,11 +1,14 @@
-
+# Coursera and Johns Hopkins University
+# Data Science Specialization
+# Course04 : Exploratory Data Analysis
+# Project01 : ExData_Plotting1
+# 2019-02-27
 
 data_raw <- read.table(file="household_power_consumption.txt",head=TRUE,sep=";",colClasses=c("character","character","character","character","character","character","character","character","character"))
 # only be using data from the dates 2007-02-01 and 2007-02-02.
 data_part <- subset(data_raw, Date=="1/2/2007" | Date=="2/2/2007")
 # missing values are coded as ?.
 data <- data_part[!apply(data_part, 1, function(x) {any(x == "?")}),]
-
 
 data$Date<-as.Date(data$Date,"%d/%m/%Y")
 data$Time <- chron(times=data$Time)
@@ -17,7 +20,12 @@ data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
 data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
 data$Sub_metering_3<-as.numeric(data$Sub_metering_3)
 
-
-
+assign("data", data, envir = .GlobalEnv)
 # head(data)
-is.null(data_part)
+
+
+
+source("plot1.R")
+plot1(data)
+
+
