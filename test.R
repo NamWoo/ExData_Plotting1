9bc0d305c9504996e2e51965fd66b1487310fdb8
Nam Woo <preice@yonsei.ac.kr>
2019-02-27 09:46:51 +0900

    add

---
+++ test.R
@@ -0,0 +1,23 @@
+
+
+data_raw <- read.table(file="household_power_consumption.txt",head=TRUE,sep=";",colClasses=c("character","character","character","character","character","character","character","character","character"))
+# only be using data from the dates 2007-02-01 and 2007-02-02.
+data_part <- subset(data_raw, Date=="1/2/2007" | Date=="2/2/2007")
+# missing values are coded as ?.
+data <- data_part[!apply(data_part, 1, function(x) {any(x == "?")}),]
+
+
+data$Date<-as.Date(data$Date,"%d/%m/%Y")
+data$Time <- chron(times=data$Time)
+data$Global_active_power<-as.numeric(data$Global_active_power) 
+data$Global_reactive_power<-as.numeric(data$Global_reactive_power) 
+data$Voltage<-as.numeric(data$Voltage) 
+data$Global_intensity<-as.numeric(data$Global_intensity)
+data$Sub_metering_1<-as.numeric(data$Sub_metering_1)
+data$Sub_metering_2<-as.numeric(data$Sub_metering_2)
+data$Sub_metering_3<-as.numeric(data$Sub_metering_3)
+
+
+
+# head(data)
+is.null(data_part)
